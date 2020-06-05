091cf95cccaca4789733b6efe14078acefb7ff2d
Susan Holland <segh@fastmail.com>
2020-06-05 17:17:33 -0400

    Assigned variables to arrays, added functions to get criteria

--- script.js
+++ script.js
@@ -2,71 +2,69 @@
 var generateBtn = document.querySelector("#generate");
 
 //Assign variables to arrays with lower and uppercase letters, numbers, and special characters.
-
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(lowerCase.length);
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+console.log(upperCase.length);
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+console.log(numbers.length);
+var specialChar = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+console.log(specialChar.length);
 
 // Functions to get criteria via prompts (length, lowercase, uppercase, numbers, special characters).
 
 function getLength() {
   // Set variable to prompt response
-
+  var length = prompt("Choose password length by entering number between 8 and 128");
   // Validate user input
-
+  if ((length >= 8) && (length <= 128)) {
     // Return variable if validated
-  return 20;
+    return length;
+  }
   // Else return alert
-
+  return alert("Length not valid, please try again");
 }
+console.log(getLength());
 
-function askLower() {
-  // Set variable to prompt response
+// Function to confirm characters, validate responses, and create super array
+function getCharTypes() {
+  // Declare super array
+  var superArray = [];
 
-  // Validate user input
+  // Three confirms set to variables
+  var lower = confirm("Click Okay to use lowercase characters in password, or Cancel to exclude them");
+  var upper = confirm("Click Okay to use uppercase characters in password, or Cancel to exclude them");
+  var number = confirm("Click Okay to use numbers in password, or Cancel to exclude them");
+  var special = confirm("Click Okay to use special characters in password, or Cancel to exclue them");
 
-    // Return variable if validated
+  // Validate that at least one confirm is true
+  if ((!lower) && (!upper) && (!number) && (!special)) {
+    alert("Must select at least one character type, please try again");
+  }
 
-  // Else return alert
+  // Concat selected arrays to superArray
+  if (lower) {
+    superArray = superArray.concat(lowerCase);
+  }
+  if (upper) {
+    superArray = superArray.concat(upperCase);
+  }
+  if (number) {
+    superArray = superArray.concat(numbers);
+  }
+  if (special) {
+    superArray = superArray.concat(specialChar);
+  }
 
+  console.log(superArray.length);
+  return superArray;
 }
-
-function askUpper() {
-  // Set variable to prompt response
-
-  // Validate user input
-
-    // Return variable if validated
-
-  // Else return alert
-
-}
-
-function askNum() {
-  // Set variable to prompt response
-
-  // Validate user input
-
-    // Return variable if validated
-
-  // Else return alert
-
-}
-
-function askChar() {
-  // Set variable to prompt response
-
-  // Validate user input
-
-    // Return variable if validated
-
-  // Else return alert
-
-}
+console.log(getCharTypes());
 
 // Function to randomly generate password based on criteria entered
 function generatePassword() {
   // Define variables set to criteria prompt functions
   var length = getLength();
-
-  // If/else statements using prompt responses and three previously defined arrays to create super array
 
   // Run for loop for number of times the length of password
 
