30b4007bdfae22e7402f2e37d4a17a2209516191
Susan Holland <segh@fastmail.com>
2020-06-06 13:23:03 -0400

    Added character array of type objects

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var superArray = [];
-var lowerCase = { 
-  name: "lowercase letters", 
-  isSelected: true, 
-  array: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
-};
-var upperCase = { 
-  name: "uppercase letters", 
-  isSelected: true,  
-  array: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
-};
-var numbers = { 
-  name: "numbers", 
-  isSelected: true,  
-  array: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
-};
-var specialChar = { 
-  name: "special characters", 
-  isSelected: true, 
-  array: ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."] 
-};
+var characters = [
+  { 
+    name: "lowercase letters", 
+    isSelected: true, 
+    array: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
+  },
+  { 
+    name: "uppercase letters", 
+    isSelected: true,  
+    array: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+  },
+  { 
+    name: "numbers", 
+    isSelected: true,  
+    array: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+  },
+  { 
+    name: "special characters", 
+    isSelected: true, 
+    array: ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."] 
+  }
+];
 
 // Function to prompt user to select password length
 function getLength() {
   // Set variable to prompt response
-  var length = prompt("Choose password length by entering number between 8 and 128");
+  var length = prompt("Choose your password length: \n\nEnter a number between 8 and 128");
   // If user selects Cancel...
   if (length === null) {
     return;
@@ -34,12 +36,12 @@ function getLength() {
     return length;
   }
   // Else return alert
-  return alert("Length not valid, please try again");
+  return alert("Please try again! \n\nLength must be a number >= 8 and <= 128");
 }
 
 // Function to confirm character type and push to superArray if true
 function getCharacters(type) {
-  type.isSelected = confirm("Click Okay to use " + type.name + " in password or Cancel to exclude them");
+  type.isSelected = confirm("Click OK to use " + type.name + " in password \n\nClick Cancel to exclude them");
   if (type.isSelected) {
     superArray = superArray.concat(type.array);
   }
@@ -49,15 +51,14 @@ function getCharacters(type) {
 function generatePassword() {
   // Assign variable to result string
   var result = "";
+
   // Run length prompt and set to variable...
   var length = getLength();
-  // While length is valid, run getCharacters with character type arguments
+  // While length is valid, run getCharacters for each type in character array
   while (length !== undefined) {
-    getCharacters(lowerCase);
-    getCharacters(upperCase);
-    getCharacters(numbers);
-    getCharacters(specialChar);
-
+    for (var i = 0; i < characters.length; i++) {
+      getCharacters(characters[i]);
+    }
     // If superArray isn't empty...
     if (superArray.length > 0) {
       // Run for loop for number of times the length of password...
